13ab63dab93eacd0acb4e6b660b17514ff823b8e
bulldogman <andrevdcolff@me.com>
2015-11-07 18:54:04 +0000

    Fixing code for autograder#7

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,7 +5,7 @@ class MoviesController < ApplicationController
   end
 
   def show
-    reset_session
+    #reset_session
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
