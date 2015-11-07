132988acd42e090b6d1e8c769bbd3e4ac2d53d4f
bulldogman <andrevdcolff@me.com>
2015-11-07 14:18:29 +0000

    Homework part 1 done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,13 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @sort_by = params[:sort]
+    
+    if @sort_by == nil
+      @movies = Movie.all
+    else
+      @movies = Movie.order(@sort_by)
+    end
   end
 
   def new
